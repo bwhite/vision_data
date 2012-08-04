4d644243910f1c4ef5878a36836af97020720992
Brandyn A. White <bwhite@dappervision.com>
2012-08-04 19:34:48 -0400

    Added api key/secret to call
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vision_data/hadoop/__init__.py
+++ vision_data/hadoop/__init__.py
@@ -6,7 +6,11 @@ def _lf(fn):
     return os.path.join(__path__[0], fn)
 
 
-def flickr_images(tags, images_per_tag, hdfs_output):
+def flickr_images(tags, images_per_tag, hdfs_output, api_key=None, api_secret=None):
+    if api_key is None or api_secret is None:
+        api_key = os.environ['FLICKR_API_KEY']
+        api_secret = os.environ['FLICKR_API_SECRET']
     hadoopy.writetb(hdfs_output + '/tags', ((images_per_tag, x) for x in tags))
-    hadoopy.launch_frozen(hdfs_output + '/tags', hdfs_output + '/metadata', _lf('flickr_bulk.py'))
+    hadoopy.launch_frozen(hdfs_output + '/tags', hdfs_output + '/metadata', _lf('flickr_bulk.py'), cmdenvs={'FLICKR_API_KEY': api_key,
+                                                                                                            'FLICKR_API_SECRET': api_secret})
     hadoopy.launch_frozen(hdfs_output + '/metadata', hdfs_output + '/image_metadata', _lf('file_downloader.py'))
