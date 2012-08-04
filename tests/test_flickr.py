90b8b264b7a0518597300c707e95a758b4e68096
Brandyn A. White <bwhite@dappervision.com>
2012-08-03 22:36:01 -0400

    Added flickr
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ tests/test_flickr.py
@@ -0,0 +1,22 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_flickr(self):
+        import vision_data
+        d = vision_data.Flickr()
+        import pprint
+        pprint.pprint(zip(range(10), d.image_class_parse_url('pepsi logo')))
+
+if __name__ == '__main__':
+    unittest.main()
+
