d26c1c412a91371195df302dd7fcf8eb1983d24c
Brandyn A. White <bwhite@dappervision.com>
2012-08-04 20:00:57 -0400

    Added chunks to tags
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vision_data/hadoop/__init__.py
+++ vision_data/hadoop/__init__.py
@@ -6,15 +6,22 @@ def _lf(fn):
     return os.path.join(__path__[0], fn)
 
 
-def flickr_images(tags, images_per_tag, hdfs_output, api_key=None, api_secret=None, remove_output=False):
+def _chunks(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+def flickr_images(tags, images_per_tag, hdfs_output, num_files=20, api_key=None, api_secret=None, remove_output=False):
+    tags = list(tags)
     if api_key is None or api_secret is None:
         api_key = os.environ['FLICKR_API_KEY']
         api_secret = os.environ['FLICKR_API_SECRET']
+    tags_per_chunk = max(len(tags) / num_files, 1)
     if remove_output and hadoopy.exists(hdfs_output):
         print('Removing output dir[%s]' % hdfs_output)
         hadoopy.rmr(hdfs_output)
-    for tag_num, tag in enumerate(tags):
-        hadoopy.writetb(hdfs_output + '/tags/%d' % tag_num, [(images_per_tag, tag)])
+    for chunk_num, chunk_tags in enumerate(_chunks(tags, tags_per_chunk)):
+        hadoopy.writetb(hdfs_output + '/tags/%d' % chunk_num, [(images_per_tag, tag) for tag in chunk_tags])
     hadoopy.launch_frozen(hdfs_output + '/tags', hdfs_output + '/metadata', _lf('flickr_bulk.py'), cmdenvs={'FLICKR_API_KEY': api_key,
                                                                                                             'FLICKR_API_SECRET': api_secret})
     hadoopy.launch_frozen(hdfs_output + '/metadata', hdfs_output + '/image_metadata', _lf('file_downloader.py'))
