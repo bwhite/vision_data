e7db0c5a106352ae691ab561bb05a2f76c5d4cb7
Brandyn A. White <bwhite@dappervision.com>
2013-04-30 00:23:17 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ vision_data/_lfwfunneling.py
@@ -0,0 +1,19 @@
+import vision_data
+import glob
+import os
+
+
+class LFWFunneling(vision_data.VisionDataset):
+
+    def __init__(self):
+        super(LFWFunneling, self).__init__(name='lfwfunneling',
+                                           data_urls={('1b42dfed7d15c9b2dd63d5e5840c86ad',
+                                                       'lfw-funneled.tgz'): ['http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz']},
+                                           homepage='http://vis-www.cs.umass.edu/lfw/',
+                                           bibtexs=None,
+                                           overview=None)
+
+    def images(self):
+        for p in glob.glob(self.dataset_path + '/lfw_funneled/*'):
+            for y in glob.glob(p + '/*'):
+                yield '', os.path.basename(p), {'meta:class': os.path.basename(y), 'data:image': open(y).read()}
