e17891c26f8c31c765331cd3809c8d3b33dfce0c
Brandyn A. White <bwhite@dappervision.com>
2013-05-05 00:01:05 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vision_data/_caltech256.py
+++ vision_data/_caltech256.py
@@ -20,5 +20,5 @@ class Caltech256(vision_data.VisionDataset):
     def images(self):
         for class_name_enc in glob.glob(self.dataset_path + '/256_ObjectCategories/*'):
             class_name = re.search(r'.+256_ObjectCategories/[0-9]+\.([a-z\-]+[a-z])(\-101)?', class_name_enc).group(1)
-            for fn in glob.glob(class_name_enc + '/*'):
+            for fn in glob.glob(class_name_enc + '/*.jpg'):
                 yield '', os.path.basename(fn), {'meta:class': class_name, 'data:image': open(fn).read()}
