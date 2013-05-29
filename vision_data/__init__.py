10d865526935f78662d8d9aeb14665aa9a02ae35
Brandyn A. White <bwhite@dappervision.com>
2013-05-28 22:24:18 -0400

    Added verbose
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vision_data/__init__.py
+++ vision_data/__init__.py
@@ -22,18 +22,19 @@ DATASETS = {'CIFAR10': CIFAR10, 'SUN397': SUN397,
 TABLE = 'images'
 
 
-def hbase_loader(prefix, dataset, thrift_server, thrift_port):
+def hbase_loader(prefix, dataset, thrift_server, thrift_port, verbose=False):
     import hadoopy_hbase
     dataset = DATASETS[dataset]()
     client = hadoopy_hbase.connect(thrift_server, thrift_port)
     for split, name, columns in dataset.images():
         row = hadoopy_hbase.hash_key(name, prefix=prefix + split, suffix=name, hash_bytes=4)
-        print(repr(row))
+        if verbose:
+            print(repr(row))
         mutations = [hadoopy_hbase.Mutation(column=x, value=y) for x, y in columns.items()]
         client.mutateRow(TABLE, row, mutations)
 
 
-def picarus_loader(prefix, dataset, email, picarus_server, api_key=None, login_key=None, otp=None, download=False, test=False):
+def picarus_loader(prefix, dataset, email, picarus_server, api_key=None, login_key=None, otp=None, download=False, test=False, verbose=False):
     import hadoopy_hbase
     import picarus
     dataset = DATASETS[dataset]()
@@ -46,7 +47,8 @@ def picarus_loader(prefix, dataset, email, picarus_server, api_key=None, login_k
     client = picarus.PicarusClient(email=email, api_key=api_key, server=picarus_server, max_attempts=10)
     for split, name, columns in dataset.images():
         row = hadoopy_hbase.hash_key(name, prefix=prefix + split, suffix=name, hash_bytes=4)
-        print('row[%r] len(data:image)[%d]' % (repr(row), len(columns.get('data:image', ''))))
+        if verbose:
+            print('row[%r] len(data:image)[%d]' % (repr(row), len(columns.get('data:image', ''))))
         client.patch_row(TABLE, row, columns)
         if test:
             remote_columns = client.get_row(TABLE, row)
