4c5aee13705f319bfb0f7ebb66632d2b95e7fd38
Brandyn A. White <bwhite@dappervision.com>
2013-05-28 20:24:27 -0400

    Moved loaders into the main library
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vision_data/__init__.py
+++ vision_data/__init__.py
@@ -15,3 +15,45 @@ from _cifar100 import CIFAR100
 from _imagenet import ImageNet
 from _labelme import LabelMe
 from _flickr import Flickr
+
+
+DATASETS = {'CIFAR10': CIFAR10, 'SUN397': SUN397,
+            'LFWFunneling': LFWFunneling, 'Caltech256': Caltech256}
+TABLE = 'images'
+
+
+def hbase_loader(prefix, dataset, thrift_server, thrift_port):
+    import hadoopy_hbase
+    dataset = DATASETS[dataset]()
+    client = hadoopy_hbase.connect(thrift_server, thrift_port)
+    for split, name, columns in dataset.images():
+        row = hadoopy_hbase.hash_key(name, prefix=prefix + split, suffix=name, hash_bytes=4)
+        print(repr(row))
+        mutations = [hadoopy_hbase.Mutation(column=x, value=y) for x, y in columns.items()]
+        client.mutateRow(TABLE, row, mutations)
+
+
+def picarus_loader(prefix, dataset, email, picarus_server, api_key=None, login_key=None, otp=None, download=False, test=False):
+    import hadoopy_hbase
+    import picarus
+    dataset = DATASETS[dataset]()
+    if download:
+        dataset.download()
+    if otp:
+        api_key = picarus.PicarusClient(email=email, login_key=login_key, server=picarus_server).auth_yubikey(otp)['apiKey']
+    if api_key is None:
+        raise ValueError('api_key or login_key/otp must be set!')
+    client = picarus.PicarusClient(email=email, api_key=api_key, server=picarus_server, max_attempts=10)
+    for split, name, columns in dataset.images():
+        row = hadoopy_hbase.hash_key(name, prefix=prefix + split, suffix=name, hash_bytes=4)
+        print('row[%r] len(data:image)[%d]' % (repr(row), len(columns.get('data:image', ''))))
+        client.patch_row(TABLE, row, columns)
+        if test:
+            remote_columns = client.get_row(TABLE, row)
+            if remote_columns != columns:
+                print(remote_columns)
+                print(columns)
+                print({x: len(y) for x, y in remote_columns.items()})
+                print({x: len(y) for x, y in columns.items()})
+                assert remote_columns == columns
+            client.delete_row(TABLE, row)
---
+++ vision_data/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+import vision_data
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Vision data loader')
+    subparser = parser.add_subparser('picarus', help='Load database using Picarus')
+    subparser.add_argument('prefix')
+    subparser.add_argument('dataset', choices=list(vision_data.DATASETS))
+    subparser.add_argument('email')
+    subparser.add_argument('--download', action='store_true')
+    subparser.add_argument('--api_key')
+    subparser.add_argument('--login_key')
+    subparser.add_argument('--otp')
+    subparser.add_argument('--test', help='Upload, get, check, delete each row (one row at a time).', action='store_true')
+    subparser.add_argument('--picarus_server', default='https://api.picar.us')
+    subparser.set_defaults(func=vision_data.picarus_loader)
+
+    subparser = subparser.add_subparser('hbase', help='Load database using Picarus')
+    subparser.add_argument('prefix')
+    subparser.add_argument('dataset', choices=list(vision_data.DATASETS))
+    subparser.add_argument('--download', action='store_true')
+    subparser.add_argument('--thrift_server', default='localhost')
+    subparser.add_argument('--thrift_port', default='9090')
+    subparser.set_defaults(func=vision_data.hbase_loader)
+
+    args = subparser.parse_args()
+    kw = vars(args)
+    del kw['func']
+    args.func(**kw)
+
+if __name__ == '__main__':
+    main()
